d1bc9f2fd427e02b6f5b4d87ab6f73c06356e74d
Mohammad <67665004+mohammadhlm@users.noreply.github.com>
2024-03-04 09:39:30 -1000

    Update account_tmp.js

--- src/account_tmp.js
+++ src/account_tmp.js
@@ -1,5 +1,3 @@
-export const nearAccountMainnetID = "here";
-export const nearAccountMainnetPK = "here";
-export const ethEvmosAddress = "here"; //free address (you can use your address or anyone) , ex : 0x00000000219ab540356cBB839Cbe05303d7705Fa
+
 export const strkAddress = "here"; 
 export const strkContractAddress = "0x04718f5a0Fc34cC1AF16A1cdee98fFB20C31f5cD61D6Ab07201858f4287c938D";  //dont change this
